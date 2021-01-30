1361311e2b6912302f6322acf1b73a805ad13a6b
toms1441 <d3nw0@tuta.io>
2021-01-31 00:16:53 +0300

    color centering

--- main.go
+++ main.go
@@ -2,30 +2,46 @@ package main
 
 import (
 	"fmt"
+	"math"
 	"strings"
 
 	"gopkg.in/gookit/color.v1"
 )
 
-const template = "\x1b[38;2;0;255;0;48;2;%d;%d;%dmTRUECOLOR\x1b[0m\n"
-
 func main() {
+	// color names
 	for _, v := range colors {
 		clr := p.Get500(v)
 
 		rgb := color.HexToRGB(clr)
 		r, g, b := uint8(rgb[0]), uint8(rgb[1]), uint8(rgb[2])
 
-		x := 6 - len(v)
+		x := (6 - len(v))
+
+		space := " "
 		if x > 0 {
-			v += strings.Repeat(" ", x)
+			space = strings.Repeat(space, x)
+		}
+		name := v
+
+		v = space + v + space
+		if len(name) <= 6 && len(v) > 8 {
+			v = v[:8]
+		} else if len(name) <= 5 && len(v) < 8 {
+			v += strings.Repeat(" ", 8-len(v))
 		}
 
-		str := color.RGB(r, g, b, false).Sprint(v)
-		fmt.Print(str, " ")
+		bg := color.RGB(255, 255, 255, false)
+		if ((float64(r)*0.299 + float64(g)*0.587 + float64(b)*0.114) / 255) > 0.5 {
+			bg = color.RGB(0, 0, 0, false)
+		}
+
+		fg := color.RGB(r, g, b, true)
+		bg.Print(fg.Sprint(v))
 	}
 	fmt.Println()
 
+	// actual colors
 	for _, s := range order {
 		for _, v := range colors {
 			if len(p[v][s]) > 0 {
@@ -37,14 +53,20 @@ func main() {
 					fg = color.RGB(0, 0, 0, false)
 				}
 
-				space := " "
-				x := len(v) - 6
-				if x > 0 {
-					space = strings.Repeat(space, x+1)
+				left := " "
+				right := " "
+
+				fl := float64((len(v) - 6)) / 2
+				if fl > 0 {
+					x := int(fl)
+					left = strings.Repeat(left, x+1)
+					if math.Mod(fl, 1) != 0 {
+						x++
+					}
+					right = strings.Repeat(right, x+1)
 				}
 
-				str := fg.Sprint(color.RGB(r, g, b, true).Sprint(p[v][s][1:]))
-				fmt.Print(str, space)
+				fg.Print(color.RGB(r, g, b, true).Sprint(left, p[v][s][1:], right))
 			}
 		}
 		fmt.Println()
