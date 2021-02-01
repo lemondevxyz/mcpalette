e1be97e57cad1860ddb7e608fa0b5b8be118f3f2
toms1441 <d3nw0@tuta.io>
2021-02-01 18:14:02 +0300

    add -rgb, -hsl, -n flags

--- main.go
+++ main.go
@@ -8,19 +8,33 @@ var monochrome = false
 var colorfilter = ""
 
 func main() {
+
+	f := FormatHEX
+	rgb := false
+	hsl := false
 	{
 		const fu = "display the hex codes but without color"
 		flag.BoolVar(&monochrome, "no-color", false, fu)
-		flag.BoolVar(&monochrome, "nc", false, fu+" (shorthand)")
+		flag.BoolVar(&monochrome, "n", false, fu+" (shorthand)")
 	}
-	/*
-		{
-			const fu = "display rgb codes instead of hex codes"
-			flag.StringVar(&colorfilter, "color", "")
-		}
-	*/
+	{
+		const fu = "display rgb instead of hex codes"
+		flag.BoolVar(&rgb, "rgb", false, fu)
+		flag.BoolVar(&rgb, "r", false, fu+" (shorthand)")
+	}
+	{
+		const fu = "display hsl instead of hex codes"
+		flag.BoolVar(&hsl, "hsl", false, fu)
+		flag.BoolVar(&hsl, "h", false, fu+" (shorthand)")
+	}
 	flag.Parse()
 
+	if rgb == true {
+		f = FormatRGB
+	} else if hsl == true {
+		f = FormatHSL
+	}
+
 	//print_palette(FormatRGB, material)
-	print_palette(FormatHSL, material)
+	print_palette(f, material)
 }
